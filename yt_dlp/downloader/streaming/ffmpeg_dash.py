d67dc4433bc657edd3bf2ecee4865b286913202b
rad3sh <30727293+rad3sh@users.noreply.github.com>
2026-09-17 02:39:10 -0300

    fix: route DASH segments to output directory

--- yt_dlp/downloader/streaming/ffmpeg_dash.py
+++ yt_dlp/downloader/streaming/ffmpeg_dash.py
@@ -12,10 +12,14 @@ class FFmpegDashOutput:
         self.logger = logger
         self.process = subprocess.Popen([
             'ffmpeg', '-hide_banner', '-loglevel', 'error', '-f', 'mpegts', '-i', 'pipe:0',
-            '-map', '0', '-c', 'copy', '-f', 'dash', '-seg_duration', '2',
+            '-map', '0:v:0', '-map', '0:a:0', '-c', 'copy', '-tag:v', 'avc1', '-tag:a', 'mp4a',
+            '-bsf:a', 'aac_adtstoasc',
+            '-f', 'dash', '-seg_duration', '2',
             '-streaming', '1', '-window_size', '0', '-extra_window_size', '0',
-            '-remove_at_exit', '0', '-init_seg_name', 'init-$RepresentationID$.mp4',
-            '-media_seg_name', 'chunk-$RepresentationID$-$Number$.m4s',
+            '-remove_at_exit', '0', '-init_seg_name',
+            str(self.directory / 'init-$RepresentationID$.mp4'),
+            '-media_seg_name',
+            str(self.directory / 'chunk-$RepresentationID$-$Number$.m4s'),
             str(self.directory / 'manifest.mpd'),
         ], stdin=subprocess.PIPE, stdout=subprocess.DEVNULL, stderr=subprocess.PIPE)
 
@@ -23,8 +27,12 @@ class FFmpegDashOutput:
         if fragment.get('is_init_segment'):
             return
         if self.process.stdin:
-            self.process.stdin.write(data)
-            self.process.stdin.flush()
+            try:
+                self.process.stdin.write(data)
+                self.process.stdin.flush()
+            except BrokenPipeError as err:
+                error = self.process.stderr.read().decode(errors='replace') if self.process.stderr else ''
+                raise RuntimeError(f'FFmpeg DASH transmux stopped: {error.strip()}') from err
 
     def finalize(self):
         if self.process.stdin:
